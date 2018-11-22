4100468cd75f9faad88c2d66a2e52f01ecbaf51e
kuiguo1991 <kuiguo1991@gmail.com>
2018-11-22 13:42:05 +0800

    Updates

---
+++ precache-manifest.09c402652ff07397186f681d9f02bada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d27c321de604519248",
+    "url": "/react-demo/static/js/runtime~main.d6d27c32.js"
+  },
+  {
+    "revision": "24526fade84982e8b109",
+    "url": "/react-demo/static/js/main.24526fad.chunk.js"
+  },
+  {
+    "revision": "3b7d7130cb56913041e8",
+    "url": "/react-demo/static/js/1.3b7d7130.chunk.js"
+  },
+  {
+    "revision": "24526fade84982e8b109",
+    "url": "/react-demo/static/css/main.e9101e94.chunk.css"
+  },
+  {
+    "revision": "3b7d7130cb56913041e8",
+    "url": "/react-demo/static/css/1.b8c79ca9.chunk.css"
+  },
+  {
+    "revision": "4ed0738089a0bf7e94af76b3978a4b6a",
+    "url": "/react-demo/index.html"
+  }
+];
