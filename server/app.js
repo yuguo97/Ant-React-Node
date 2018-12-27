d9dd58fa50f93e1b9400c4d16f8c453272bffff2
kuiguo1991 <kuiguo1991@gmail.com>
2018-12-28 00:11:58 +0800

    提交2018/12/28#01

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
@@ -9,18 +11,28 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
-
-var sql = 'SELECT * FROM websites';
-//查
-connection.query(sql, function (err, result) {
-    if (err) {
-        console.log('[SELECT ERROR] - ', err.message);
-        return;
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  
+    if (req.method == 'OPTIONS') {
+      res.send(200); 
     }
+    else {
+      next();
+    }
+  });
 
-    console.log('--------------------------SELECT----------------------------');
-    console.log(result);
-    console.log('------------------------------------------------------------\n\n');
-});
 
-connection.end();
+app.get('/v3/api', (req, res) => {
+    const sqlStr = 'SELECT * FROM websites ';
+    connection.query(sqlStr, (err, results) => {
+        if (err) return res.json({ code: 1, message: '资料不存在', affextedRows: 0 })
+        res.json({ code: 200, result: results, affextedRows: results.affextedRows })
+    })
+})
+
+app.listen(7553, () => {
+    console.log('正在监听端口3000,http://192.168.0.116:7553/v3/api'); //192.168.1.114换成你的ip,本机ip查询用cmd=>ipconfig
+})
--- src/pages/news/News.js
+++ src/pages/news/News.js
@@ -1,15 +1,69 @@
-
-import React from 'react';
-import { Spin , Card } from 'antd';
+import React from 'react'
+import { Spin } from 'antd';
 import Breadcrumb from "./../../components/Breadcrumb";
+// 引入 ECharts 主模块
+import echarts from 'echarts/lib/echarts';
+// 引入柱状图
+import  'echarts/lib/chart/pie';
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/title';
+import 'echarts/lib/component/legend';
+
 import axios from "axios/index";
-const { Meta } = Card;
+
+function getPie(data){
+    var name=[];
+    for(var i=0;i<data.length;i++){
+        name.push(data[i].name);
+    }
+    var myChart = echarts.init(document.getElementById('pie'));
+    var option = {
+        title : {
+            text: '某站点用户访问来源',
+            subtext: '纯属虚构',
+            x:'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            x: 'left',
+            data:name
+        },
+        series: [
+            {
+                name:'访问来源',
+                type:'pie',
+                radius: ['50%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'outer'
+                    },
+                    emphasis: {
+                        show: true,
+                        textStyle: {
+                            fontSize: '30',
+                            fontWeight: 'bold'
+                        }
+                    }
+                },
+                data:data
+            }
+        ]
+    };
+    window.onresize = myChart.resize;
+    myChart.setOption(option);
+}
 
 class News extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            data:[],
             loading: false
         }
     }
@@ -17,42 +71,37 @@ class News extends React.Component {
         this.setState({
             loading:true
         });
-        axios.get(`https://www.apiopen.top/journalismApi`)
+        axios.get(`http://192.168.0.116:7553/v3/api`)
             .then(res => {
-                console.log(res.data.data.money);
                 this.setState({
-                    data:res.data.data.money,
-                    loading: false
-                })
+                    loading:false
+                });
+                var resData=res.data.result;
+                var data=[];
+                for(var  i = 0;i<resData.length;i++){
+                    data.push({
+                        "name":resData[i].name,
+                        "value":resData[i].alexa
+                    })
+                }
+                console.log(data);
+                getPie(data);
             });
     }
     render() {
         return (
-            <div className="aNews">
-                <Breadcrumb name="新闻数据" />
+            <div className="aAbout">
+                <Breadcrumb name="图表数据" />
                 <div className="Content">
-                    <Spin spinning={this.state.loading} tip="Loading...">
-                        {
-                            this.state.data.map((item, key) => (
-                                <Card
-                                    hoverable
-                                    key={key}
-                                    className="aNewsItem"
-                                    cover={<img alt={item.source} src={item.picInfo[0].url} style={{ height: 280 }} />}
-                                >
-                                    <Meta
-                                        title={item.digest}
-                                        description={item.ptime}
-                                    />
-                                </Card>
-                            ))
-                        }
-
+                    <Spin tip="Loading..." spinning={this.state.loading}>
+                        <div id="pie" className="pie" />
                     </Spin>
                 </div>
             </div>
-        );
+        )
     }
 }
+
+
 
 export default News
